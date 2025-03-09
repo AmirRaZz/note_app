7609b3137bedda6053ea4114e43b333878360b5e
RaZz <amirrahmati471@gmail.com>
2025-03-09 21:35:46 +0330

    add delete and complete note

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,31 @@ import NoteList from "./components/NoteList";
 import { Note } from "./types/Note";
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
 
-  const [notes, setNotes] = useState<Note[] >([]);
-  const handleAddNote=(newNote:Note)=>{
-    setNotes(prevNotes=>[...prevNotes,newNote])
-  }
+  const handleAddNote = (newNote: Note) => {
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  };
+
+  const handleDeleteNote = (id: number) => {
+    // const filteredNotes=notes.filter(note=>note.id !== id);
+    // setNotes(filteredNotes)
+    setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
+  };
+
+  const handleCompleteNote = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const noteId = Number(e.target.value);
+    // const newNotes = notes.map((note) =>
+    //   note.id === noteId ? { ...note, completed: !note.completed } : note
+    // );
+    // setNotes(newNotes);
+    setNotes((prevNotes) =>
+      prevNotes.map((note) =>
+        note.id === noteId ? { ...note, completed: !note.completed } : note
+      )
+    );
+  };
+
   return (
     <>
       <div className="container">
@@ -17,7 +37,11 @@ function App() {
         <div className="note-app">
           <AddNewNote onAddNote={handleAddNote} />
           <div className="note-container">
-            <NoteList/>
+            <NoteList
+              notes={notes}
+              onDelete={handleDeleteNote}
+              onComplete={handleCompleteNote}
+            />
           </div>
         </div>
       </div>
--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,8 +1,64 @@
+import { Note } from "../types/Note";
 
-function NoteList() {
-  return <div>Note list</div>
+type props = {
+  notes: Note[];
+  onDelete: (id: number) => void;
+  onComplete: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+function NoteList({ notes, onDelete, onComplete }: props) {
+  return (
+    <div className="note-list">
+      {notes.map((note) => (
+        <NoteItem
+          key={note.id}
+          note={note}
+          onDelete={onDelete}
+          onComplete={onComplete}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default NoteList;
 
+function NoteItem({
+  note,
+  onDelete,
+  onComplete,
+}: {
+  note: Note;
+  onDelete: (id: number) => void;
+  onComplete: (e:React.ChangeEvent<HTMLInputElement>) => void;
+}) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
 
+  return (
+    <div className={`note-item ${note.completed ? "completed":""}`}>
+      <div className="note-item__header">
+        <div>
+          <p className="title">{note.title}</p>
+          <p className="desc">{note.description}</p>
+        </div>
+        <div className="actions">
+          <button onClick={() => onDelete(note.id)}>❌</button>
+          <input
+            type="checkbox"
+            name={String(note.id)}
+            id={String(note.id)}
+            value={note.id}
+            checked={note.completed}
+            onChange={onComplete}
+          />
+        </div>
+      </div>
+      <div className="note-item__footer">
+        {new Date(note.createdAt).toLocaleDateString("en-US", options)}
+      </div>
+    </div>
+  );
+}
